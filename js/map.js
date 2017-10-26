e23b7b0f599f2bf0279d6eafa3f17cc9369466f8
Margarida Salvado <margaridasalvado@MacBook-Pro-de-Margarida.local>
2017-10-26 14:30:30 +1100

    add basic maps and page styles

---
+++ js/map.js
@@ -0,0 +1,22 @@
+var map; 
+// color styles inspired by https://snazzymaps.com/style/93/lost-in-the-desert 
+var styles = [
+             {
+               featureType: 'water',
+                 stylers: [{color: '#1994bf'}]
+             },
+             {
+                featureType: 'landscape',
+                 stylers: [{color: '#f9ddc5'},
+                           {lightness: -7}
+                          ]
+             }
+         ];
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -33.9051, lng:151.1551},
+        zoom: 15, 
+        styles: styles
+    }); 
+}
