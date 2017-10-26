cfb54155d3be6411595df708bba7eba80a2d3315
Margarida Salvado <margaridasalvado@MacBook-Pro-de-Margarida.local>
2017-10-26 15:27:02 +1100

    add markers and move initMap function to main.js

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ var locations = [ //ko.observableArray ??????
         type: ['establishment', 'food']
     },
     {
-        position: {lat: ,lng: }, 
+        position: {lat:  -33.911442,lng: 151.155324}, 
         name: 'Banana Joe\'s Foodworks',
         placeId: 'ChIJ5ZjmhmOwEmsRbLQh6yyI8P8',
         type: ['establishment', 'food', 'grocery_or_supermarket']
@@ -49,6 +49,49 @@ var locations = [ //ko.observableArray ??????
     }
 ]; 
 
+
+var map; 
+var markers = [];
+// color styles inspired by https://snazzymaps.com/style/93/lost-in-the-desert 
+var styles = [
+             {
+               featureType: 'water',
+                 stylers: [{color: '#1994bf'}]
+             },
+             {
+                featureType: 'landscape',
+                 stylers: [{color: '#f9ddc5'},
+                           {lightness: -7}
+                          ]
+             }
+         ];
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -33.9051, lng: 151.1551},
+        zoom: 15, 
+        styles: styles
+    }); 
+    
+// Uses the location array in main.js to create an array of markers
+    for(var i = 0; i < locations.length; i++) {
+        //Get data from locations array 
+        var position = locations[i].position; 
+        var name = locations[i].name; 
+        //create the marker itself
+        var marker = new google.maps.Marker({
+            map: map,
+            id: i,
+            position: position,
+            name: name
+            // animation: google.maps.Animation.DROP, -- see documentation before doing animation
+        }); 
+        //push the marker created above into the markers array
+        markers.push(marker);
+        
+    }
+    
+}
 var Model = function() {
     //locations
 }
